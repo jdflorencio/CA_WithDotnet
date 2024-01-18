2b95fe0d9facc39102bc5002a02ca9246dda5550
João Diego Florencio <jdflorencio@gmail.com>
2024-01-18 06:26:54 -0300

    feat

---
+++ Persistence/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchiteture.Persistence.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base (options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
