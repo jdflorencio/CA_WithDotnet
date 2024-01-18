fb90046294975c4a784062b893c338badeb784b8
João Diego Florencio <jdflorencio@gmail.com>
2024-01-18 06:42:13 -0300

    feat: repositorio generico

--- Domain/Entities/BaseEntity.cs
+++ Domain/Entities/BaseEntity.cs
@@ -9,6 +9,7 @@ namespace CleanArchitecture.Domain.Entities
     public abstract class BaseEntity
     {
         public Guid ID { get; set; }
+        public Guid Id { get; set; }
         public DateTimeOffset DateCreated { get; set; }
         public DateTimeOffset? DateUpdated { get; set; }
         public DateTimeOffset? DateDeleted { get; set; }
