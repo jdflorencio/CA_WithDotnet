13a1414f40b53a9a7fa3d8ecf5bd09fb6f6f5789
João Diego Florencio <jdflorencio@gmail.com>
2024-01-23 12:44:42 -0300

    feat:update

---
+++ CleanArchitecture.Application/UseCases/UpdateUser/UpdateUserResponse.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Application.UseCases.CreateUser;
+
+
+namespace CleanArchitecture.Application.UseCases.UpdateUser;
+
+public record UpdateUserResponse 
+{
+    public Guid Id { get; set; }
+    public string? Email { get; set; }
+    public string? Name { get; set; }
+}
